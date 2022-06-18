6a9c20195191d2330f119cb95a6e45793a85a67a
ianrg90 <iangladulich@gmail.com>
2022-06-18 12:50:06 +1000

    CSS and Bootstrap

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -3,8 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   //template can be passed like this:
-  //template: <app-server></app-server><app-server></app-server>
+  //template: `<app-server></app-server><app-server></app-server>`
   templateUrl: './servers.component.html',
+  //The same goes to styles, you can call style props and write the styles directly here
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
