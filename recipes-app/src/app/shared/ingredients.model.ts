08d27f6912fd3d7fc16be0248513814e37be1bba
ianrg90 <iangladulich@gmail.com>
2022-06-21 14:13:29 +1000

    fix

---
+++ recipes-app/src/app/shared/ingredients.model.ts
@@ -0,0 +1,17 @@
+/*
+export class Ingredient {
+    public name: string;
+    public ammount: number;
+
+    constructor(name: string, ammount: number) {
+        this.name = name;
+        this.ammount = ammount
+    }
+}
+*/
+
+//Alternative sytax
+
+export class Ingredient {
+    constructor(public name: string, public ammount: number){}
+}
